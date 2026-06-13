c9fc1fb2b249da8515f5714aca298f4620a15cfd
majdijalal0 <majdijalal0@gmail.com>
2026-06-13 20:07:25 +0100

    cors change

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config(); 
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL, 
+  credentials: true
+}));
+
 const connectdb = require('./config/dbconnect.js');
 const authRoutes = require('./routes/authRoutes');
 const workerRoutes = require('./routes/workerRoutes');
@@ -13,10 +18,7 @@ connectdb();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: process.env.FRONTEND_URL, 
-  credentials: true
-}));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/workers', workerRoutes);
 app.use('/api/bookings', bookingRoutes);
